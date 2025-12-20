dca6d11d7f6d8dac94279c90717a2532654828ed
Lorenzo Arcioni <lorenzo.arcioni2000@gmail.com>
2025-12-20 14:53:06 +0100

    okey

--- src/evaluation/__init__.py
+++ src/evaluation/__init__.py
@@ -11,19 +11,19 @@ from .metrics import (
     print_verification_results,
 )
 
-from .visualization import (
-    # Training visualizations
-    plot_training_history,
-    plot_triplet_training_history,
-    plot_multiple_models_comparison,
-    
-    # Biometric visualizations
-    plot_biometric_results,
-    plot_confusion_matrix,
-    
-    # Reports
-    create_training_report,
-)
+#from .visualization import (
+#    # Training visualizations
+#    plot_training_history,
+#    plot_triplet_training_history,
+#    plot_multiple_models_comparison,
+#    
+#    # Biometric visualizations
+#    plot_biometric_results,
+#    plot_confusion_matrix,
+#    
+#    # Reports
+#    create_training_report,
+#)
 
 __all__ = [
     # Metrics
