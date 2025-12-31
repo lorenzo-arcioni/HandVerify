2d6ee0d41bf2e73bdf8f56e3eca364faab62112c
Lorenzo Arcioni <lorenzo.arcioni2000@gmail.com>
2025-12-31 02:43:20 +0100

    ok

--- src/training/trainer_triplet.py
+++ src/training/trainer_triplet.py
@@ -36,7 +36,7 @@ class TripletTrainer(BaseTrainer):
         self.optimizer = torch.optim.AdamW(
             self.model.parameters(),
             lr=1e-4,
-            weight_decay=1e-4,
+            weight_decay=1e-2,
             betas=(0.9, 0.999)
         )
         
