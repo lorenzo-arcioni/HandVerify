8aac2b87383ad8abc5cd6959e19d92421de20ae7
Lorenzo Arcioni <lorenzo.arcioni2000@gmail.com>
2026-09-09 01:31:09 +0200

    ok

---
+++ report/compile.sh
@@ -0,0 +1,4 @@
+pdflatex -interaction=nonstopmode -file-line-error sn-article.tex
+bibtex sn-article
+pdflatex -interaction=nonstopmode -file-line-error sn-article.tex
+pdflatex -interaction=nonstopmode -file-line-error sn-article.tex
